aec1a41a4ef2f9bc6540c568135a9509fd91454e
Faith Sabu <faithsabu1791@gmail.com>
2023-10-21 12:17:04 +0530

    connect to mongodb

--- api/index.js
+++ api/index.js
@@ -1,4 +1,15 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+// mongoose.connect("mongodb+srv://faithsabu:Faith@plot-a-plot.nyd8drq.mongodb.net/?retryWrites=true&w=majority")
+mongoose.connect(process.env.MONGO).then(()=> {
+  console.log("Connected to MongoDB");
+}).catch((err)=> {
+  console.log(err);
+})
 
 const app = express();
 

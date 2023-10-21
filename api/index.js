13ab5b955345c3fd8c6d497c95daf2282ff32658
Faith Sabu <faithsabu1791@gmail.com>
2023-10-21 13:49:27 +0530

    create a test api route

--- api/index.js
+++ api/index.js
@@ -1,10 +1,10 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from 'dotenv'
+import userRouter from './routes/user.route.js'
 
 dotenv.config()
 
-// mongoose.connect("mongodb+srv://faithsabu:Faith@plot-a-plot.nyd8drq.mongodb.net/?retryWrites=true&w=majority")
 mongoose.connect(process.env.MONGO).then(()=> {
   console.log("Connected to MongoDB");
 }).catch((err)=> {
@@ -18,3 +18,5 @@ const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
+app.use("/api/user", userRouter)

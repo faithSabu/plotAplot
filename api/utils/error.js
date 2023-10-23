20c4e01c59bc4592bf6f2694d2d89414b74135bd
Faith Sabu <faithsabu1791@gmail.com>
2023-10-23 14:37:18 +0530

    create user profile update api

--- api/utils/error.js
+++ api/utils/error.js
@@ -2,5 +2,7 @@ export const errorHandler = (statusCode, message) => {
   const error = new Error();
   error.statusCode = statusCode;
   error.message = message;
+  console.log(error,'err');
   return error;
 };
+  

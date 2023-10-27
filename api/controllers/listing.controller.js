aa48311d39bc692bd094f32f6bd07e24eff6ddd7
Faith Sabu <faithsabu1791@gmail.com>
2023-10-27 21:12:33 +0530

    complete update user-listing route api

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -28,3 +28,21 @@ export const deleteListing = async (req, res, next) => {
     next(error);
   }
 };
+
+export const editListing = async (req, res, next) => {
+  const listing = await Listing.findById(req.params.id);
+  console.log(listing);
+  if (!listing) return next(errorHandler(404, "Listing not Found!"));
+  if (req.user.id !== listing.userRef)
+    return next(errorHandler(401, "You can only edit own Listing!"));
+  try {
+    const updatedListing = await Listing.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
+    res.status(200).json(updatedListing);
+  } catch (error) {
+    next(error);
+  }
+};

f7cc4e0d121709279d428126b99bafa231925d2f
Faith Sabu <faithsabu1791@gmail.com>
2023-10-28 13:05:31 +0530

    add image slider in listing page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,6 +10,7 @@ import Header from "./components/Header";
 import Signup from "./pages/Signup";
 import CreateListing from "./pages/CreateListing";
 import UpdateListing from "./pages/UpdateListing";
+import Listing from "./pages/Listing";
 
 export default function App() {
   return (
@@ -21,10 +22,14 @@ export default function App() {
         <Route path="/sign-up" element={<Signup />} />
         <Route path="/sign-out" element={<Signout />} />
         <Route path="/about" element={<About />} />
+        <Route path="/listing/:listingId" element={<Listing />} />
         <Route element={<PrivateRoute />}>
           <Route path="/profile" element={<Profile />} />
           <Route path="/create-listing" element={<CreateListing />} />
-          <Route path="/update-listing/:listingId" element={<UpdateListing />} />
+          <Route
+            path="/update-listing/:listingId"
+            element={<UpdateListing />}
+          />
         </Route>
       </Routes>
     </BrowserRouter>
---
+++ client/src/pages/Listing.jsx
@@ -0,0 +1,64 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { Swiper, SwiperSlide } from "swiper/react";
+import SwiperCore from "swiper";
+import { Navigation } from "swiper/modules";
+import "swiper/css/bundle";
+
+export default function Listing() {
+  SwiperCore.use([Navigation]);
+  const params = useParams();
+
+  const [listing, setListing] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    const listingId = params.listingId;
+    try {
+      const getListing = async () => {
+        setLoading(true);
+        const res = await fetch(`/api/listing/getListing/${listingId}`);
+        const data = await res.json();
+        if (data.success === false) {
+          setError(data.message);
+          setLoading(false);
+          return;
+        }
+        setListing(data);
+        setLoading(false);
+        setError(false);
+      };
+      getListing();
+    } catch (error) {
+      setLoading(false);
+      setError(error.message);
+    }
+  }, [params.listingId]);
+
+  return (
+    <main>
+      {loading && <p className="text-center my-7 text-2xl">Loading...</p>}
+      {error && (
+        <p className="text-center my-7 text-2xl text-red-700">{error}</p>
+      )}
+      {listing && !loading && !error && (
+        <>
+          <Swiper navigation>
+            {listing.imageURLs.map((url) => (
+              <SwiperSlide key={url}>
+                <div
+                  className="h-[550px]"
+                  style={{
+                    background: `url(${url}) center no-repeat`,
+                    backgroundSize: "cover",
+                  }}
+                ></div>
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        </>
+      )}
+    </main>
+  );
+}

1871e77c4e33be44d809e66fbfbecacaf3e7d9ab
Faith Sabu <faithsabu1791@gmail.com>
2023-10-31 14:16:57 +0530

    complete about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,7 +1,29 @@
-import React from 'react'
+import React from "react";
 
 export default function About() {
   return (
-    <div className='text-black'>About</div>
-  )
+    <div className="py-20 px-4 max-w-6xl mx-auto">
+      <h1 className="text-3xl font-bold mb-4 text-slate-800">About <span className="text-slate-600">Plot-A-Plot</span></h1>
+      <p className="mb-4 text-slate-700">
+        Welcome to Plot-A-Plot, a platform dedicated to showcasing graceful
+        houses available for rent or sale. Our focus is on connecting seekers
+        with their ideal living spaces in the most sought-after neighborhoods.
+        We're not just an agency; we're a team of passionate experts committed
+        to making your property journey hassle-free.
+      </p>
+      <p className="mb-4 text-slate-700">
+        Our goal is to guide clients towards their dream properties through
+        tailored advice and a profound understanding of the local market.
+        Whether you're buying, selling, or renting, we're here for you at every
+        turn, ensuring a seamless experience.
+      </p>
+      <p className="mb-4 text-slate-700">
+        With a team rich in real estate expertise, we pride ourselves on
+        delivering excellent service. We believe that navigating the real estate
+        market should be enjoyable and fulfilling for everyone. At Plot-A-Plot,
+        we're devoted to turning the excitement of finding or selling a property
+        into a reality for each of our clients.
+      </p>
+    </div>
+  );
 }

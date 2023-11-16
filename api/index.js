4f65ef662eb0b9d90dedee4c123feb4b724b8e36
Faith Sabu <faithsabu1791@gmail.com>
2023-11-17 00:20:12 +0530

    pre installation

--- api/index.js
+++ api/index.js
@@ -5,12 +5,15 @@ import cookieParser from "cookie-parser";
 import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
 import listingRouter from "./routes/listing.route.js";
+import path from "path";
 
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
 dotenv.config();
+
+const __dirname = path.resolve();
 
 mongoose
   .connect(process.env.MONGO)
@@ -30,6 +33,12 @@ app.listen(PORT, () => {
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/listing", listingRouter);
+
+app.use(express.static(path.join(__dirname, "/client/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 
 // Error handling middleware
 app.use((err, req, res, next) => {
